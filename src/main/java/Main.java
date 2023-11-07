09db86d4e6570774f4095f8209e9ea48d5a9d4d4
YuriS <yuri-s@list.ru>
2023-11-07 22:33:26 +0300

    After code formated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,10 @@ public class Main {
         long amount = 5000;       // Стоимость покупки
         boolean registered = true; // Зарегистрированный пользователь
 
-       long expected = 50;     // Ожидаемое число начисленных бонусщв
+        long expected = 50;     // Ожидаемое число начисленных бонусщв
         long actual = service.calculate(amount, registered);   // Фактическое число начисленных бонусщв
 
-        System.out.println( expected + " == ? == " + actual );    //  Вывод результата сравнения в терминал
+        System.out.println(expected + " == ? == " + actual);    //  Вывод результата сравнения в терминал
 
     }
 }
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,8 +1,8 @@
-        import org.junit.jupiter.api.Test;
-        import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-    //public class BonusServiceTest {
-        public class BonusServiceTest {
+//public class BonusServiceTest {
+public class BonusServiceTest {
 
     @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
